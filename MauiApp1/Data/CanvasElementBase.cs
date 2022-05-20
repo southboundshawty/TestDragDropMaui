fa5ae3e4aae6c6ec3adec04104929e3c5ac0edfd
Tsybal <N64g3Fq7>
2022-05-20 18:21:02 +0300

    ddavd

--- MauiApp1/Data/CanvasElementBase.cs
+++ MauiApp1/Data/CanvasElementBase.cs
@@ -1,25 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MauiApp1.Data
+﻿namespace MauiApp1.Data
 {
-    public class Rectangle
+    internal class CanvasElementBase : ICanvasElement
     {
-        public int RectangleId { get; set; }
+        public int Id { get; set; }
         public int OriginalX { get; set; }
         public int OriginalY { get; set; }
         public int X { get; 
             set; }
-        public int Y { get;
+        public int Y { get; 
             set; }
         public int Width { get; set; }
         public int Height { get; set; }
-        public int R { get; set; }
-        public int G { get; set; }
-        public int B { get; set; }
 
         public string Position
         {
---
+++ MauiApp1/Data/ICanvasElement.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiApp1.Data
+{
+    public interface ICanvasElement
+    {
+        public int Id { get; set; }
+        public int OriginalX { get; set; }
+        public int OriginalY { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string Position { get; }
+    }
+}
