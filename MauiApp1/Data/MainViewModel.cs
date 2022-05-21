3263e4d64b27abc8a2ca4875832a0d5542ea6fa3
Tsybal <N64g3Fq7>
2022-05-21 09:57:46 +0300

    dsdsfds

--- MauiApp1/Data/MainViewModel.cs
+++ MauiApp1/Data/MainViewModel.cs
@@ -4,12 +4,10 @@
     {
         public int ContainerX { get; set; } = 0;
         public int ContainerY { get; set; } = 0;
-        public int ElementX { get; set; } = 0;
-        public int ElementY { get; set; } = 0;
         public int ContainerMouseX { get; set; } = 0;
         public int ContainerMouseY { get; set; } = 0;
         public bool OverChild { get; set; } = false;
-        public int SelectedItem { get; set; } = -1;
+        public ICanvasElement SelectedItem { get; set; }
         public List<ICanvasElement> CanvasElements { get; set; }
         public MainViewModel()
         {
